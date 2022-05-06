01d9104686d288240948fddf45fb6c038a268646
hdavidlp <heclop@hotmail.com>
2022-05-06 10:28:02 -0700

    Con framework  para CRUD generico

--- Config/config.php
+++ Config/config.php
@@ -3,4 +3,10 @@
     CONST BASE_URL = "http://localhost/betel04/";
     // CONST LIBS = "Libraries/";
     // CONST VIEWS = "Views/";
+
+    const DB_HOST = "localhost";
+    const DB_NAME = "dblaboratory";
+    const DB_USER = "root";
+    const DB_PASSWORD = "";
+    const DB_CHARSET = "charset=utf8";
 ?>
--- Controllers/Home.php
+++ Controllers/Home.php
@@ -19,6 +19,31 @@
             $this->views->getView($this,"home", $data);
         }
 
+        public function insertar(){
+            $data = $this->model->setUser("Carlos", 18);
+            print_r($data);
+        }
+
+        public function verUsuario($id){
+            $data = $this->model->getUser($id);
+            print_r($data);
+        }
+
+        public function actualizar(){
+            $data = $this->model->updateUser(1,"Roberto", 20);
+            print_r($data);
+        }
+
+        public function verusuarios(){
+            $data = $this->model->getUsers();
+            print_r($data);
+        }
+
+        public function eliminarUsuario($id){
+            $data = $this->model->delUser($id);
+            print_r($data);
+        }
+
     }
 
 
